48e93d7ca478a6abbe9281ad3efe043e34b9537e
Reda Tarzalt <tarzaltreda@gmail.com>
2025-10-17 01:10:44 -0400

    update script

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd apps/web
-npx serve -s dist -l 3001 &
-WEB_PID=$!
 
-cd apps/mcp
+cd /app/apps/mcp
 PORT=3000 npm run serve &
 MCP_PID=$!
+
+cd /app/apps/web
+npx serve -s dist -l 3001 &
+WEB_PID=$!
 
 wait $MCP_PID $WEB_PID
