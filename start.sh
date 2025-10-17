6fa6291cda4b23f01a51ce72b4b8a4f767447123
Reda Tarzalt <tarzaltreda@gmail.com>
2025-10-17 00:29:48 -0400

    change script

--- start.sh
+++ start.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-echo "Starting all services!!!"
-
-# Start MCP server
-cd apps/mcp
-PORT=3000 npm run serve &
-MCP_PID=$!
-
-# Start web app
-cd ../web
-npm run serve &
-WEB_PID=$!
-
-# Wait for both processes
-wait $MCP_PID $WEB_PID
+cd apps/web
+npm run serve -- --port 3000 &
+wait
